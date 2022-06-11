6beb650eced84ee58667d1c2122dc9a009cb84fc
Tony Bowden <tony@tmtm.com>
2022-06-11 09:17:26 +0300

    Update to latest external-cabinet

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed on the site
+SKIP = [
+].freeze
+
 diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
-puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
+                                                 .reject { |row| SKIP.include? row }
+
+puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
